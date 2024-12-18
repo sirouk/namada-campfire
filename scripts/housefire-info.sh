fd77106e443be6f589b960ea632a4fbb75158f0e
sirouk <8901571+sirouk@users.noreply.github.com>
2024-12-18 04:55:05 +0000

    housefire info

--- scripts/housefire-info.sh
+++ scripts/housefire-info.sh
@@ -15,6 +15,7 @@ source $HOME/housefire.env
 
 
 # extract info from configs
+export BASE_DIR=${BASE_DIR:-$HOME/.local/share/namada}
 export CAMPFIRE_CHAIN_DATA="$HOME/chaindata/namada-1"
 export CHAINDATA_PATH=${CHAINDATA_PATH:-$CAMPFIRE_CHAIN_DATA}
 export FOUND_CHAIN_ID=$(awk -F'=' '/default_chain_id/ {gsub(/[ "]/, "", $2); print $2}' "$CHAINDATA_PATH/../global-config.toml")
@@ -34,9 +35,20 @@ echo "PERSISTENT_PEERS=$PERSISTENT_PEERS" >> $ENV_FILENAME
 
 echo "Updating Config for landing page..."
 export HTML_PATH="/usr/share/nginx/html/index.html"
-cp $HOME/namada-campfire/docker/config/local-namada/index-housefire.html $HTML_PATH
+cp -f $HOME/namada-campfire/html/index-housefire.html $HTML_PATH
+
+# Update the HTML file
+sed -i "s#https://testnet.DOMAIN/CHAIN_ID.tar.gz#$NAMADA_NETWORK_CONFIGS_SERVER/$CHAIN_ID.tar.gz#g" $HTML_PATH
+sed -i "s#https://testnet.DOMAIN/configs#$NAMADA_NETWORK_CONFIGS_SERVER#g" $HTML_PATH
 sed -i "s/CHAIN_ID/$CHAIN_ID/g" $HTML_PATH
 sed -i "s/NAMADA_TAG/$NAMADA_TAG/g" $HTML_PATH
 sed -i "s/DOMAIN/$DOMAIN/g" $HTML_PATH
 sed -i "s/CHAIN_PREFIX/$CHAIN_PREFIX/g" $HTML_PATH
 sed -i "s#PEER#$PERSISTENT_PEERS#g" $HTML_PATH
+
+
+# compress the directory /root/.local/share/namada/housefire-alpaca.cc0d3e0c033be/wasm as wasm.tar.gz
+tar -czf /usr/share/nginx/html/wasm.tar.gz -C $HOME/.local/share/namada/$CHAIN_ID/ wasm
+
+# make the first snapshot
+DOMAIN_PREFIX="testnet.$CHAIN_PREFIX" CHAINDATA_PATH=$BASE_DIR/$CHAIN_ID $HOME/namada-campfire/scripts/make-snapshot.sh
