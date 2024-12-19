20e2c82fe5fde0d88b510cb09976e5438b2ddc2c
sirouk <8901571+sirouk@users.noreply.github.com>
2024-12-19 03:38:41 +0000

    install yq

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -47,6 +47,7 @@ sudo apt -y update
 
 # install misc dependencies
 sudo apt -y install jq git curl ca-certificates gnupg lz4
+sudo snap install yq
 
 # Add dockerco package repository
 sudo mkdir -m 0755 -p /etc/apt/keyrings
@@ -91,9 +92,11 @@ sudo apt -y update
 echo "**************************************************************************************"
 echo "Installing LetsEncrypt/Certbot"
 echo "**************************************************************************************"
-sudo apt -y install certbot python3 python3-pip python3-certbot-nginx
+sudo apt -y install python3 python3-pip
 
-sudo apt -y --only-upgrade install certbot
 sudo apt -y --only-upgrade install python3-urllib3
 sudo python3 -m pip uninstall urllib3
 sudo python3 -m pip install urllib3
+
+sudo python3 -m pip install pyopenssl --upgrade
+sudo python3 -m pip install certbot certbot-nginx --upgrade 
