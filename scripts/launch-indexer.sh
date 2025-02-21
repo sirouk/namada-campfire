2955afedb52130d19ee1b41adc793624bbac18f6
sirouk <8901571+sirouk@users.noreply.github.com>
2025-02-21 21:10:42 +0000

    forceful overwrite

--- scripts/launch-indexer.sh
+++ scripts/launch-indexer.sh
@@ -24,7 +24,7 @@ git pull
 # git pull
 
 # Copy the docker compose file for the db
-cp $HOME/namada-campfire/docker/compose/docker-compose-db.yml $HOME/namada-indexer/docker-compose-db.yml
+cp -f $HOME/namada-campfire/docker/compose/docker-compose-db.yml $HOME/namada-indexer/docker-compose-db.yml
 
 # prep are vars
 export WIPE_DB=${WIPE_DB:-false}
