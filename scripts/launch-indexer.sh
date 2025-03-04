752b56e13856421d8aa2ba35eb085d0e4eed73e0
sirouk <8901571+sirouk@users.noreply.github.com>
2025-03-04 13:51:38 +0000

    specific indexer

--- scripts/launch-indexer.sh
+++ scripts/launch-indexer.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+62!$I68YuEJBIrs8#!/usr/bin/env bash
 
 
 # Grab the repo
@@ -10,13 +10,14 @@ cd $HOME/namada-indexer && git fetch --all
 #git checkout master
 #cd $HOME/namada-indexer && git fetch --all && git checkout chore/update-namada-to-0.43.0 && git pull
 #cd $HOME/namada-indexer && git fetch --all && git checkout patch-2 && git pull
-#git checkout tags/v1.0.1
 
 # Get the latest tag
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 git checkout $LATEST_TAG
 git reset --hard $LATEST_TAG
 git pull
+
+git checkout tags/v2.1.0
 
 #git checkout fix-transparent-tx-kind
 #git pull
