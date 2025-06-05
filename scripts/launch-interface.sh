f394128a1916f8f83652f467e40a6bd3b9bbda51
sirouk <8901571+sirouk@users.noreply.github.com>
2025-06-05 20:00:20 +0000

    update interface launch for branch

--- scripts/launch-interface.sh
+++ scripts/launch-interface.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 # Examples:
-    #Mainnet:
-    # CHAINDATA_PATH=$BASE_DIR ~/namada-campfire/scripts/launch-interface.sh
+    # Mainnet and Housefire:
+    # unset BRANCH; CHAINDATA_PATH=$BASE_DIR ~/namada-campfire/scripts/launch-interface.sh
+
+    # use a branch
+    # BRANCH=namadillo@v1.21.0 CHAINDATA_PATH=$BASE_DIR ~/namada-campfire/scripts/launch-interface.sh
 
     # Campfire
-    # CHAINDATA_PATH=$HOME/chaindata/namada-1 ~/namada-campfire/scripts/launch-interface.sh
+    # unset BRANCH; CHAINDATA_PATH=$HOME/chaindata/namada-1 ~/namada-campfire/scripts/launch-interface.sh
 
 # Run this script after the chain is running to write the namada-interface .env file, rebuild and start the container.
 # Note: as this rebuilds the container it takes some time to complete
 REPO_NAME="namada-interface"
 REPO_DIR="$HOME/$REPO_NAME"
 INTERFACE_DIR="apps/namadillo"
+export BRANCH=${BRANCH:-""}
 
 
 ### Grab the repo
@@ -26,9 +30,14 @@ cd $REPO_DIR && git fetch --all
 #git checkout tags/v1.0.4
 
 # Get the latest tag
-LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-git checkout $LATEST_TAG
-git reset --hard $LATEST_TAG
+if [ -z "${BRANCH}" ]; then
+    LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+    git checkout $LATEST_TAG
+    git reset --hard $LATEST_TAG
+else
+    git checkout $BRANCH
+    git reset --hard $BRANCH
+fi
 git pull
 
 #git checkout main
